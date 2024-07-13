24c22499c8748a9c0abf043b0edb3260e5d080f0
Alperenakkal <alperenakkal06@gmail.com>
2024-07-13 14:47:15 +0300

    Created Configurations

---
+++ Infrastructure/HepsiApi.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using HepsiApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Persistence.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            Category category1 = new()
+            {
+                Id = 1,
+                Name = "Elektirik",
+                Priorty= 0,
+            }
+        }
+    }
+}
